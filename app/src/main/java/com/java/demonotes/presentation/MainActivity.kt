c358e8cce666ff574aa3caadba473aa998954371
DINESH G <gdineshg029@gmail.com>
2022-01-03 12:21:18 +0530

    done with the project setup of dependencies

---
+++ app/src/main/java/com/java/demonotes/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.java.demonotes.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.java.demonotes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
